1fd96657745b75bb59ff176b1d25e9afc7abf3f2
gavuolo <gabs.vuolo@gmail.com>
2022-10-06 11:31:58 -0400

    primeiro commit

---
+++ src/components/Film.js
@@ -0,0 +1,15 @@
+export default function Film(){
+
+    ruturn(
+        <>
+            <div className="film"> o filme aqui</div>
+            <div className="film"> o filme aqui</div>
+            <div className="film"> o filme aqui</div>
+            <div className="film"> o filme aqui</div>
+            <div className="film"> o filme aqui</div>
+            <div className="film"> o filme aqui</div>
+            <div className="film"> o filme aqui</div>
+        </>
+       
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header(){
+
+    return(
+        <>
+        <div className="top-bar">
+            <h1>CINEFLEX</h1>
+        </div>
+        <div className="select-film">
+            <p>Selecionar filme</p>
+        </div>
+        </>
+    )
+
+}
+
+
